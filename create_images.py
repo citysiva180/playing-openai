f365715c04de2dc2acdd10bb34d41df10a6f6319
Sivarajan <citysiva180@users.noreply.github.com>
2022-11-25 10:12:09 +0530

    image-generator

---
+++ create_images.py
@@ -0,0 +1,29 @@
+import openai
+from api_secrets import API_KEY
+
+openai.api_key = API_KEY
+
+
+def create_image_on_prompt(prompt_sentence):
+
+    print(prompt_sentence)
+    try:
+        response = openai.Image.create(
+            prompt=prompt_sentence,
+            n=1,
+            size="512x512"
+        )
+
+        image_url = response['data'][0]['url']
+        return image_url
+
+    except openai.error.OpenAIError as e:
+
+        print(e.http_status)
+        print(e.error)
+
+
+
+output = create_image_on_prompt(
+    "princess diana as a disney princess realistic")
+print(output)
